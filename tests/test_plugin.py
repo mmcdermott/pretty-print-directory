ea20b3c4cf8e183a1f803c228d4e4752a9162221
Matthew McDermott <mattmcdermott8@gmail.com>
2025-05-07 11:50:14 -0400

    Adding printconfig

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -8,7 +8,8 @@ importlib.reload(pretty_print_directory.pytest_plugin)
 def test_plugin_doctest():
     """This tests the plugin code via a doctest.
 
-    Note that there is no import of `print_directory` here -- it should be automatically added by the plugin.
+    Note that there is no import of `print_directory` or `PrintConfig`, here -- they should be automatically
+    added by the plugin.
 
     Examples:
         >>> with tempfile.TemporaryDirectory() as tmpdir:
@@ -22,6 +23,14 @@ def test_plugin_doctest():
         │   └── baz.csv
         ├── file1.txt
         └── foo
+        >>> with tempfile.TemporaryDirectory() as tmpdir:
+        ...     path = Path(tmpdir)
+        ...     (path / "file1.txt").touch()
+        ...     (path / "foo").mkdir()
+        ...     (path / "bar").mkdir()
+        ...     (path / "bar" / "baz.csv").touch()
+        ...     print_directory(path, config=PrintConfig(file_extension=[".txt"]))
+        └── file1.txt
     """
 
     pass
